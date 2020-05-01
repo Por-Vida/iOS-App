322b505080c0f1cfbebdf30fd085232d0d0ab253
Jonathan Rosett <jrose06@jaguar.tamu.edu>
2020-05-01 16:38:44 -0500

    Adding pod files

---
+++ Pods/Target Support Files/Pods-PorVidaMobile-PorVidaMobileUITests/Pods-PorVidaMobile-PorVidaMobileUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PorVidaMobile_PorVidaMobileUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PorVidaMobile_PorVidaMobileUITests
+@end
---
+++ Pods/Target Support Files/Pods-PorVidaMobile/Pods-PorVidaMobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PorVidaMobile : NSObject
+@end
+@implementation PodsDummy_Pods_PorVidaMobile
+@end
