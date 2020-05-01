1a5b9c765e57b227659bfe873e8ade01e00fc70c
Jonathan Rosett <jrose06@jaguar.tamu.edu>
2020-05-01 17:02:36 -0500

    Added the basics of the map screen

---
+++ PorVidaMobile/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  PorVidaMobile
+//
+//  Created by Jonathan on 5/1/20.
+//  Copyright © 2020 BigByteDevelopment. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
