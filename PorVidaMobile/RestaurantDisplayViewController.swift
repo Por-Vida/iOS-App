8f7ad2bc5e579b69af8f5eb0a72b2fa7ddade48c
Rey <reycrittenden@yahoo.com>
2020-05-03 17:22:19 -0500

    Fixed crashes that occurred due to OutOfBounds and more accurately displayed restaurant separators

--- PorVidaMobile/RestaurantDisplayViewController.swift
+++ PorVidaMobile/RestaurantDisplayViewController.swift
@@ -13,14 +13,15 @@ import Parse
 
 class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var tmp = 0
+    
     var restaurantObj = [PFObject]()
-    var restaurant: [PFObject]!
-    var orderedRest = [PFObject]()
-    var restClone = [PFObject]()
+    var rest: [PFObject]!
+    var ordered = [PFObject]()
     var currentRestaurant: PFObject! = nil
     var rowCount = [Int]()
+    var arr = [String]()
     var displayCount = 0
-    //var restaurants = [PFObject]()
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -39,6 +40,7 @@ class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UI
     func updateTableview() {
         let pull = PFQuery(className: "Restaurant")
         pull.includeKeys(["name", "managerLast", "managerFirst", "street", "city", "state", "zip", "Meals"])//I edited this
+        
 
         do {
             try restaurantObj = pull.findObjects()
@@ -46,48 +48,55 @@ class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UI
             print("ERROR: \(error)")
         }
         
-        restaurant = restaurantObj
+        rest = restaurantObj
         
+        
+        
+        var i = restaurantObj.count - 1
         var place = 0
         
-        while !restaurant.isEmpty {
-            orderedRest.append(restaurant.remove(at: 0))
-            place = restaurant.count - 1
+        
+        
+        while !rest.isEmpty {
+            ordered.append(rest.remove(at: 0))
+            place = rest.count - 1
+            
             while place >= 0 {
-                //print("Place: \(place)")
-                //print("Rest: \(ordered.count)")
-                if restaurant[place]["name"] as! String == orderedRest[orderedRest.count - 1]["name"] as! String {
-                    orderedRest.append(restaurant.remove(at: place))
+                if rest[place]["name"] as! String == ordered[ordered.count - 1]["name"] as! String {
+                    ordered.append(rest.remove(at: place))
                 }
                 place -= 1
             }
         }
         
-        restClone = orderedRest
-        
-        print(orderedRest.count)
+        print(ordered.count)
         var counting = 0
-        var hold = orderedRest[0]["name"] as! String
-        for index in 0 ..< orderedRest.count {
-            if hold == orderedRest[index]["name"] as! String {
-                print("Match: \(orderedRest[index]["name"] as! String)")
+        var rest = ordered
+        var hold = ordered[0]["name"] as! String
+        
+        for index in 0 ..< ordered.count {
+            
+            if hold == ordered[index]["name"] as! String {
                 counting += 1
             } else {
-                hold = orderedRest[index]["name"] as! String
-                print(hold)
-                print("COUNTING: \(counting)")
+                arr.append(hold)
+                hold = ordered[index]["name"] as! String
+                //print(hold)
+                //print("COUNTING: \(counting)")
                 rowCount.append(counting)
                 counting = 1
             }
         }
+        arr.append(ordered[ordered.count - 1]["name"] as! String)
+        print("ARRAY: \(arr)")
         
         var max = 0
         for count in 0 ..< rowCount.count {
-            print("LOOK \(rowCount[count])")
+            //print("LOOK \(rowCount[count])")
             max += rowCount[count]
         }
-        print("MAX: \(max)")
-        rowCount.append(orderedRest.count - max)
+        //print("MAX: \(max)")
+        rowCount.append(ordered.count - max)
         
         for index in 0 ..< rowCount.count {
             print(rowCount[index])
@@ -95,63 +104,55 @@ class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UI
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //Rows based on how many of the same restaurant
-        
-        print("SECTION \(section)")
-        
         return rowCount[section] + 1
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var orderCopy = ordered
         
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "DisplayCell", for: indexPath) as! DisplayCell//Display cell will be used as "Post" cell, to hold all of the similar restaurants
-            displayCount += rowCount[indexPath.section]
-            //print("LOOKIT: \(indexPath.section)")
-//            print("ORDERED: \(rowCount[indexPath.section])")
-//            print("DISP: \(displayCount)")
-//            print("DISPLAY: \(orderedRest[displayCount - 1]["name"] as! String)")
-            print("ERROR \(displayCount - 1)")
-            
-            if displayCount - 1 < orderedRest.count {
-                cell.restaurantName.text = orderedRest[displayCount - 1]["name"] as! String
+            if displayCount - 1 < ordered.count {
+                cell.restaurantName.text = arr[indexPath.section]
             }
             
             return cell
         } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath) as! RestaurantCell
             
-            //while restClone.isEmpty {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantDisplayCell", for: indexPath) as! RestaurantDisplayCell
+            var restaurantIndex = 0
+            var index = 0
             
-            if restClone.count != 0 {
-                print("Boodoo dodo")
-                
-                let street = restClone[0]["street"] as! String
-                let city = restClone[0]["city"] as! String
-                let state = restClone[0]["state"] as! String
-                let zip = restClone[0]["zip"] as! String
-                
-                let location = "\(street), \(city), \(state), \(zip)"
-                
-                cell.locationLabel.text = location
-                
-                restClone.remove(at: 0)
-                //}
-                
-                return cell
+            if indexPath.section > 1 {
+                restaurantIndex = (indexPath.section * 2) + (indexPath.row)
+            } else if indexPath.section == 0 {
+                restaurantIndex = indexPath.row
             } else {
-                return cell
+                index = indexPath.section + 1
+                restaurantIndex = ((index * 2) + (indexPath.row)) - 1
             }
-
+            
+            let street = ordered[restaurantIndex - 1]["street"] as! String
+            let city = ordered[restaurantIndex - 1]["city"] as! String
+            let state = ordered[restaurantIndex - 1]["state"] as! String
+            let zip = ordered[restaurantIndex - 1]["zip"] as! String
+            
+            let location = "\(street), \(city), \(state), \(zip)"
+            
+            cell.restaurant.text = (ordered[restaurantIndex - 1]["name"] as! String)
+            cell.location.text = location
+            
+            orderCopy.remove(at: indexPath.row)
+            
+            return cell
         }
         
         
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        //solve the section based
         
-        let restaurant = orderedRest
+        var restaurant = ordered
         var counter = 1
         
         var held = restaurant[0]["name"] as! String
@@ -178,35 +179,11 @@ class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UI
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        if indexPath.row != 0 {
-////            print(indexPath)
-////            print("\(indexPath.section), \(indexPath.row)")
-//            let selectedCell = tableView.cellForRow(at: indexPath) as! RestaurantDisplayCell
-//
-//            var restaurantIndex = 0
-//            var index = 0
-//
-//            if indexPath.section > 1 {
-//                restaurantIndex = (indexPath.section * 2) + (indexPath.row)
-//            } else if indexPath.section == 0 {
-//                restaurantIndex = indexPath.row
-//            } else {
-//                index = indexPath.section + 1
-//                restaurantIndex = ((index * 2) + (indexPath.row)) - 1
-//            }
-//
-//            currentRestaurant = orderedRest[restaurantIndex-1]
-//            print("CURRENT PRE: \(currentRestaurant)")
-//        }
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //print("Text")
-//        let cell = sender as! UITableViewCell
-//        let indexPath = tableView.indexPath(for: cell)!
-//        let restaurantPass = orderedRest[indexPath.row]
+        //        let cell = sender as! UITableViewCell
+        //        let indexPath = tableView.indexPath(for: cell)!
+        //        let restaurantPass = orderedRest[indexPath.row]
         //let indexPath = tableView.indexPath(for: cell)!
         
         let indexPath = tableView.indexPathForSelectedRow!
@@ -215,7 +192,7 @@ class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UI
         if indexPath.row != 0 {
             //            print(indexPath)
             //            print("\(indexPath.section), \(indexPath.row)")
-            let selectedCell = tableView.cellForRow(at: indexPath) as! RestaurantDisplayCell
+            //let selectedCell = tableView.cellForRow(at: indexPath) as! RestaurantDisplayCell
             
             var restaurantIndex = 0
             var index = 0
@@ -229,7 +206,7 @@ class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UI
                 restaurantIndex = ((index * 2) + (indexPath.row)) - 1
             }
             
-            currentRestaurant = orderedRest[restaurantIndex-1]
+            currentRestaurant = ordered[restaurantIndex-1]
         }
         
         let restaurantDetails = segue.destination as! MenuDetailsViewController
@@ -237,5 +214,9 @@ class RestaurantDisplayViewController: UIViewController, UITableViewDelegate, UI
         //print("CURRENT: \(currentRestaurant)")
         //print(restaurantDetails.restaurant["name"] as! String)
     }
+    
+    
+    
+    
 }
 
