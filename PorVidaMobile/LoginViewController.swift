3e531cb325bc6e0b553744a5be9bc4de2623cc9d
Jane Perez <JaneBethPerez@gmail.com>
2020-05-06 16:51:52 -0500

    Jane RoughDraft Login

--- PorVidaMobile/LoginViewController.swift
+++ PorVidaMobile/LoginViewController.swift
@@ -7,21 +7,71 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var segmentLevel: UISegmentedControl!
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+   var authorization = "Admin"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func authLevel(_ sender: Any) {
+        switch segmentLevel.selectedSegmentIndex
+        {
+        case 0:
+            authorization = "Admin"
+        case 1:
+            authorization = "Manger"
+        default:
+            break
+        }
     }
     
     @IBAction func onBack(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func login(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        PFUser.logInWithUsername(inBackground: username, password: password ) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+             let alertController = UIAlertController(title: "Wrong Login", message:
+                 "Admins Only", preferredStyle: .alert)
+             alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+             self.present(alertController, animated: true, completion: nil)
+        }
+        }
+    }
     
-
+    @IBAction func signUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        // other fields can be set just like with PFObject
+        user["Authorization"] = authorization
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+            self.performSegue(withIdentifier: "loginSegue", sender: nil)
+        } else {
+            print("Error: \(error?.localizedDescription)")
+        }
+      }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
