f9bd403367d121feec0f68d87c51fad42972c832
Jonathan Rosett <jrose06@jaguar.tamu.edu>
2020-05-05 21:35:47 -0500

    Updated the map view to be able to show the annotations of the restaurants

--- PorVidaMobile/MapViewController.swift
+++ PorVidaMobile/MapViewController.swift
@@ -14,20 +14,18 @@ import Parse
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet var mapView: MKMapView!
-
+    
     let locationManager = CLLocationManager()
     @Published var currentLatitude: Double = 0.0
     @Published var currentLongitude: Double = 0.0
+    
     var restaurants = [PFObject]()
-    var restaurant: PFObject!
     var mapRestaurants = [mapRestaurant]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
-        
         // Enabling the location manager:
         // Requesting authorization:
         self.locationManager.requestAlwaysAuthorization()
@@ -37,47 +35,22 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
-
         // Setting up the map view
         mapView.delegate = self
         mapView.mapType = .standard
         mapView.isZoomEnabled = true
         mapView.isScrollEnabled = true
-
         
         // Creating the restaurants for the map:
-        let query = PFQuery(className: "Restaurants")
-        
+        let query = PFQuery(className: "Restaurant")
+                
         // Query to get all of the restaurants into a usable array
-        query.findObjectsInBackground { (restaurants, error) in
-            if restaurants != nil {
-                self.restaurants = restaurants!
-            }
-        }
-        
-        // For loop to create a map object for each restaurant in the array
-        for index in 0 ..< restaurants.count {
-            // Restaurant name on the map
-            let name = restaurants[index]["name"] as! String
-            
-            // Getting the restaurant's coordinates for the map
-            let lat = restaurants[index]["latitude"] as! Double
-            let long = restaurants[index]["longitude"] as! Double
-            let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
-            
-            // information is required to add an annotation
-            let information = restaurants[index]["name"] as! String
-            
-            // Creating the restaurant and adding it to an array
-            mapRestaurants[index] = mapRestaurant(title: name, coordinate: location, info: information)
-            
-            // Adding the Restaurants to the map
-            mapView.addAnnotation(mapRestaurants[index])
+        do {
+            restaurants = try query.findObjects()
+        } catch {
+            print("error")
         }
     }
-    
-    
-    
     
     // Function to add the user's current location to the map and center the map view on the user
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -95,10 +68,8 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let center = CLLocationCoordinate2D(latitude: currentLatitude, longitude: currentLongitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
         mapView.setRegion(region, animated: true)
-        
-        
     }
-    
+
     /*
     // MARK: - Navigation
 
@@ -109,4 +80,37 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     }
     */
 
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // For loop to create a map object for each restaurant in the array
+        for index in restaurants {
+            
+            // Restaurant name on the map
+            let name = index["name"] as! String
+            
+            // Getting the restaurant's coordinates for the map
+            let lat = index["latitude"] as! Double
+            let long = index["longitude"] as! Double
+            let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            // Testing the coordinates
+            print("location")
+            
+            // information is required to add an annotation
+            let information = index["name"] as! String
+            
+            // Creating the restaurant
+            mapRestaurants.append(mapRestaurant(title: name, coordinate: location, info: information))
+            
+            // Creating the annotation
+            let restaurantAnnotation = MKPointAnnotation()
+            restaurantAnnotation.coordinate.latitude = lat
+            restaurantAnnotation.coordinate.longitude = long
+            restaurantAnnotation.title = name
+            
+            // Adding the Restaurants to the map
+            mapView.addAnnotation(restaurantAnnotation)
+        }
+    }
 }
+
